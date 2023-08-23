9846380c7bd7bdd4a0728596c1ea40474f9cddc2
Rafael Barbosa Silva <r.ji@outlook.com>
2023-08-23 22:31:40 +0100

    Start adding style to Product component

--- src/components/catalog/product/product.tsx
+++ src/components/catalog/product/product.tsx
@@ -3,11 +3,17 @@ import { ProductProps } from "@/components/catalog/product/product.types";
 export const Product = ({ image, title, price }: ProductProps) => {
   return (
     <article className={styles.product}>
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <span className={styles.price}>${price}</span>
-      <button className={styles.add}>ADD TO CART</button>
-      <button className={styles.details}>QUICK VIEW</button>
+      <img className={styles.image} src={image} alt={title} />
+      <div className={styles.information}>
+        <h2>{title}</h2>
+        <span className={styles.price}>${price}</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles.add}>ADD TO CART</button>
+        <button className={styles.details} disabled>
+          QUICK VIEW
+        </button>
+      </div>
     </article>
   );
 };
