584221cb5072a42220febb8ccc31dd84c72ddff2
Rafael Barbosa Silva <r.ji@outlook.com>
2023-08-23 21:07:36 +0100

    Start creation of Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import styles from "./page.module.css";
 import Header from "@/components/header";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Header />
-    </main>
+    <>
+      <header>
+        <Header />
+        <Hero />
+      </header>
+      <main className={styles.main}></main>
+    </>
   );
 }
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,4 @@
+import styles from "./hero.module.scss";
+export const Hero = () => {
+  return <section className={styles.hero}></section>;
+};
