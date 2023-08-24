e6b87b6495f6f02492d3b75fb9ea68a0fb698ada
Rafael Barbosa Silva <r.ji@outlook.com>
2023-08-24 16:40:23 +0100

    Code refactor on style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Catalog from "@/components/catalog";
